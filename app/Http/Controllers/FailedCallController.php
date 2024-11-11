c1d8bc18f91213ee6eeded5d7c38918f1f746a33
Erik Ros <erik.ros@devilsberg.com>
2024-11-11 14:58:49 +0100

    fail endpoint

--- app/Http/Controllers/FailedCallController.php
+++ app/Http/Controllers/FailedCallController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twilio\TwiML\VoiceResponse;
+use Illuminate\Support\Facades\Log;
 
 class FailedCallController extends Controller
 {
     //
+	public function handleFail(Request $request)
+	{
+
+		if ($request->has('ErrorCode') ) {
+
+			// Log the failure details for further analysis
+			Log::error('Call failed', [
+				'CallSid' => $request->input('CallSid'),
+				'From' => $request->input('From'),
+				'To' => $request->input('To'),
+				'ErrorCode' => $request->input('ErrorCode'),
+				'ErrorMessage' => $request->input('ErrorMessage'),
+			]);
+		}
+		else {
+			Log::error('Call failed', [
+				'CallSid' => $request->input('CallSid'),
+				'From' => $request->input('From'),
+				'To' => $request->input('To'),
+			]);
+		}
+
+		$response = new VoiceResponse();
+
+		$response->say('Sorry, I did not understand your response. Please call back later.');
+
+		$response->hangup();
+
+        return response($response, 200)
+            ->header('Content-Type', 'text/xml');
+	}
 
 }
